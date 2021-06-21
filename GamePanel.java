f6c497a75c1880e6658a42f81b7d321f10d4bbd7
chanduramisetti <86049039+chanduramisetti@users.noreply.github.com>
2021-06-21 17:11:47 +0530

    Update GamePanel.java
    the updated version of the game has
    -> the background audio of the game
    -> the home page 
    -> some other details are added so that it looks better

--- GamePanel.java
+++ GamePanel.java
@@ -1,6 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.math.*;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.util.Random;
 import javax.swing.JPanel;
@@ -10,17 +15,18 @@ public class GamePanel extends JPanel implements ActionListener{
 	static final int screen_h=600;
 	static final int u_size =25;
 	static final int g_units=(screen_w*screen_h)/u_size;
-	static final int delay=75;
-	final int[] x=new int[g_units];
-	final int[] y=new int[g_units];
+ int delay=50;
+	int[] x=new int[g_units];
+	int[] y=new int[g_units];
 	int co=0;
-	int body_p=6;
+	int body_p=6,d=0;
 	int apples_e=0,apple_y,apple_x,apple_x1,apple_y1;
 	char direc ='D',m='M';
 	boolean run=false,t=false;
-	boolean b=false;
+	boolean b=false,b1=false;
 	Timer timer;
 	Random random;
+	int c=0;
 	GamePanel(){
 		random=new Random();
 		this.setPreferredSize(new Dimension(screen_w,screen_h));
@@ -32,11 +38,16 @@ public class GamePanel extends JPanel implements ActionListener{
 	public void startGame()
 	{
 		newApple();
-		run=true;
+		//run=true;
 		t=false;
+		delay=50;
 		timer=new Timer(delay,this);
 		timer.start();
-		
+		x=new int[g_units];
+		y=new int[g_units];
+		b1=false;
+		apples_e=0;
+		body_p=6;
 	}
 	public void paintComponent(Graphics g)
 	{
@@ -45,20 +56,57 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	public void draw(Graphics g)
 	{
+		if(!b&&!b1)
+		{
+			sound2();
+			b1=true;
+		}
+		if(!b)
+		{
+			g.setColor(Color.red);
+			g.setFont(new Font("Ink Free",Font.ITALIC,30));
+			FontMetrics met=getFontMetrics(g.getFont());
+			g.drawString("Click 'S' to start", (screen_w-met.stringWidth("Click 'S' to start"))/2, 300);
+			g.setFont(new Font("Ink Free",Font.ROMAN_BASELINE,40));
+			FontMetrics met1=getFontMetrics(g.getFont());
+			g.drawString("Snakeyy snakee..", (screen_w-met1.stringWidth("Snakeyy snakee..."))/2, 100);
+			g.setColor(Color.black);
+			g.setFont(new Font("Ink Free",Font.BOLD,15));
+			 met1=getFontMetrics(g.getFont());
+			g.drawString("Designed and Developed by :", (screen_w-met1.stringWidth("Desigend and Developed by :"))/2, 400);
+			g.drawString("Chandu Ramisetti", (screen_w-met1.stringWidth("Chandu Ramisetti"))/2, 420);
+			g.drawString("email : chandu978.ramisetti@gmail.com", (screen_w-met1.stringWidth("email : chandu978.ramisetti@gmail.com"))/2, 440);
+			g.drawString("contact : +91 6303859756", (screen_w-met1.stringWidth("contact : +91303859756"))/2, 460);
+			//System.out.println("controlgot_!b");
+				
+		}
+		if(b) {
 		if(run) {
 			/*
 		for(int i=0;i<screen_w/u_size;i++) {
 			g.drawLine(i*u_size,0,i*u_size, screen_h);
 			g.drawLine(0,i*u_size, screen_w,i*u_size);
 		}*/
+			if(apples_e>40)
+				timer.setDelay(delay-40);
+			else if(apples_e>30)
+				timer.setDelay(delay-30);
+			else if(apples_e>20)
+				timer.setDelay(delay-20);
+			else if(apples_e>10)
+				timer.setDelay(delay-10);
+			else
+				timer.setDelay(delay);
+			
+			
 		g.setColor(Color.green);
 		g.fillOval(apple_x, apple_y, u_size, u_size);
-		if(co%6==0&&co!=0)
+		/*if(apples_e%6==0&&apples_e!=0)
 		{
 			g.setColor(Color.orange);
-			g.fillOval(apple_x, apple_y, u_size, u_size);
+			g.fillOval(apple_x1, apple_y1, u_size, u_size);
 			
-		}
+		}*/
 		
 		
 		for(int i=0;i<body_p;i++)
@@ -66,40 +114,20 @@ public class GamePanel extends JPanel implements ActionListener{
 			if(i==0)
 			{
 				g.setColor(Color.black);
-				g.fillOval(x[i], y[i], u_size-2, u_size-2);
+				g.fillOval(x[i], y[i], u_size, u_size);
 				
 			}
 			else
 			{
-				if(b) {
-				g.setColor(Color.gray);
-				g.fillRect(x[i], y[i], u_size, u_size);
-				g.setColor(new Color(120,10,10));
-				g.fillOval(x[i]+12, y[i]+12, u_size/3,u_size/3);
-				g.setColor(new Color(120,10,10));
-				g.fillOval(x[i]+7, y[i], u_size/3,u_size/3);
-				g.setColor(new Color(120,10,10));
-				g.fillOval(x[i], y[i]+12, u_size/3,u_size/3);
-				}
-				else 
-				{
-					g.setColor(Color.gray);
-					g.fillRect(x[i], y[i], u_size, u_size);
-					g.setColor(new Color(120,10,10));
-					g.fillOval(x[i]+7, y[i]+12, u_size/3,u_size/3);
-					g.setColor(new Color(120,10,10));
-					g.fillOval(x[i], y[i], u_size/3,u_size/3);
-					g.setColor(new Color(120,10,10));
-					g.fillOval(x[i]+12, y[i], u_size/3,u_size/3);
-				}
+				g.setColor(Color.black);
+				g.fillOval(x[i], y[i], u_size, u_size);
+				
 				
 			}
 		}
 		
-	}
-		else
-			gameOver(g);
-		g.setColor(Color.black);
+g.setColor(Color.black);
+		
 		g.setFont(new Font("Ink Free",Font.ITALIC,20));
 		FontMetrics met=getFontMetrics(g.getFont());
 		g.drawString("Score: "+apples_e, (screen_w-met.stringWidth("Score: "+apples_e))/2, g.getFont().getSize());
@@ -107,72 +135,153 @@ public class GamePanel extends JPanel implements ActionListener{
 		g.setFont(new Font("Ink Free",Font.BOLD,10));
 		FontMetrics met1=getFontMetrics(g.getFont());
 		g.drawString("x-co :"+x[0]+" y-co :"+y[0]+" x_app : "+apple_x1+" y_app : "+apple_y1, 25,25);
+		
+	}
+		else {
+			gameOver(g);
+		}
+		
+	}
 	}
 	public void newApple() {
-		apple_x=random.nextInt((int)(screen_w/u_size))*u_size;
-		apple_y=random.nextInt((int)(screen_h/u_size))*u_size;
+		apple_x=random.nextInt((int)(screen_w/10))*10;
+		apple_y=random.nextInt((int)(screen_h/10))*10;
 	}
 	public void newBigAplle() {
-		apple_x1=random.nextInt((int)(screen_w/u_size))*u_size;
-		apple_y1=random.nextInt((int)(screen_h/u_size))*u_size;
+		apple_x1=random.nextInt((int)(screen_w/10))*10;
+		apple_y1=random.nextInt((int)(screen_h/10))*10;
 	}
 	public void move() {
 		for(int i=body_p;i>0;i--) {
 			x[i]=x[i-1];
 			y[i]=y[i-1];
-			b=!b;
+			d++;
+			d=d%6;
 		}
 		switch(direc) {
 		case 'U':
-			y[0]=y[0]-u_size;
+			y[0]=y[0]-10;
 			break;
 		case 'D':
-			y[0]=y[0]+u_size;
+			y[0]=y[0]+10;
 			break;
 		case 'R':
-			x[0]=x[0]+u_size;
+			x[0]=x[0]+10;
 			break;
 		case 'L':
 
-			x[0]=x[0]-u_size;
+			x[0]=x[0]-10;
 			break;
 		}
 	}
-	public void checkBig() {
-		if(x[0]==apple_x1 && y[0]==apple_y1)
+	/*public void checkBig() {
+		if(Math.abs(x[0]-apple_x1)<=10 && Math.abs(y[0]-apple_y1)<=10)
 		{
 			//timer.stop();
-			body_p++;
+			body_p+=1;
+			apples_e+=1;
+			co++;
+			}
+			
+		}*/
+	
+	public void checkApple() {
+		if(Math.abs(x[0]-apple_x)<=10 && Math.abs(y[0]-apple_y)<=10) {
+			if(apples_e>40)
+			body_p+=5;
+			else if(apples_e>30)
+				body_p+=4;
+			else if(apples_e>20)
+				body_p+=3;
+			else if(apples_e>10)
+				body_p+=2;
+			else
+				body_p++;
 			apples_e++;
 			co++;
-			
+			//timer.stop();
+			sound1();
+				newApple();
+
 		}
 	}
-	public void checkApple() {
-		if(x[0]==apple_x && y[0]==apple_y) {
-			body_p++;
-			apples_e++;
-			co++;
-			newApple();
-			b=!b;
-			if(apples_e%6==0 && apples_e!=0)
-				newBigAplle();
+	public void sound1() {
+		try {
+			File file=new File("C:\\Users\\Lenovo\\Downloads\\smb_coin.wav");
+			AudioInputStream audio=AudioSystem.getAudioInputStream(file);
+			Clip clip=AudioSystem.getClip();
+			clip.open(audio);
+			clip.start();
+		}catch(Exception e)
+		{
+			System.out.println("exception"+e);
+		}
+	}
+	public void sound2() {
+		try {
+			File file=new File("C:\\Users\\Lenovo\\Downloads\\smb_stage_clear.wav");
+			AudioInputStream audio=AudioSystem.getAudioInputStream(file);
+			Clip clip=AudioSystem.getClip();
+			clip.open(audio);
+			clip.start();
+		}catch(Exception e)
+		{
+			System.out.println("exception"+e);
+		}
+	}
+	public void sound3() {
+		try {
+			File file=new File("C:\\Users\\Lenovo\\Downloads\\smb_mariodie.wav");
+			AudioInputStream audio=AudioSystem.getAudioInputStream(file);
+			Clip clip=AudioSystem.getClip();
+			clip.open(audio);
+			clip.start();
+		}catch(Exception e)
+		{
+			System.out.println("exception"+e);
+		}
+	}
+	public void sound4() {
+		try {
+			File file=new File("C:\\Users\\Lenovo\\Downloads\\smb_jump-small.wav");
+			AudioInputStream audio=AudioSystem.getAudioInputStream(file);
+			Clip clip=AudioSystem.getClip();
+			clip.open(audio);
+			clip.start();
+		}catch(Exception e)
+		{
+			System.out.println("exception"+e);
 		}
 	}
 	public void checkCollisions() {
 		for(int i=body_p;i>0;i--)
 		{
 			if((x[0]==x[i])&&(y[0]==y[i]))
+			{
+				sound3();
 				run=false;
+			}
 		}
 		if(x[0]<0)
+		{
 			x[0]=screen_w;
+			sound4();
+		}
 		if(x[0]>screen_w)
+		{
 			x[0]=0;
+			sound4();
+		}
 		if(y[0]<0)
+		{
 			y[0]=screen_h;
+			sound4();
+		}
 		if(y[0]>screen_h)
+		{
 			y[0]=0;
+			sound4();
+		}
 		if(!run)
 			timer.stop();
 	}
@@ -187,13 +296,15 @@ public class GamePanel extends JPanel implements ActionListener{
 		g.setFont(new Font("Ink Free",Font.BOLD,20));
 		FontMetrics met1=getFontMetrics(g.getFont());
 		g.drawString("Score: "+apples_e, (screen_w-met1.stringWidth("Score: "+apples_e))/2, g.getFont().getSize());
+		g.drawString("Click Enter to Home", (screen_w-met1.stringWidth("Click Enter to Home"))/2, 400);
+		
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(run) {
 			move();
 			checkApple();
-			checkBig();
+			//checkBig();
 			checkCollisions();
 			
 		}
@@ -226,6 +337,17 @@ public class GamePanel extends JPanel implements ActionListener{
 				timer.stop();
 				else
 					timer.start();
+				break;
+			case KeyEvent.VK_S:
+				System.out.println("start");
+				b=true;
+				run=true;
+				break;
+			case KeyEvent.VK_ENTER:
+				b=false;
+				run=false;
+				startGame();
+				System.out.println("e");
 				break;
 			}
 		}
